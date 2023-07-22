affd84b5a4a38f86578e6eebbabd736c501a680e
Ingrid <git@ingrids.email>
2023-07-22 23:12:48 +0200

    set up axum

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::{IpAddr, Ipv4Addr, SocketAddr};
+
+use axum::{routing::get, Router};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+
+    let socket = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 1337);
+
+    axum::Server::bind(&socket)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
